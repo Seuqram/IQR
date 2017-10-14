2a466a9ddcc5e980f3ce05307807bdd7e887a5e9
Rodrigo Marques <rodrigo.marques@uniriotec.br>
2017-10-14 17:55:15 -0300

    Created Line class

---
+++ src/code/Line.java
@@ -0,0 +1,20 @@
+package code;
+
+public class Line {
+	private int number;
+
+	public Line(int number) {
+		super();
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+}
