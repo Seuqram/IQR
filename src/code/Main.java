9c89bb430fa331e5aa84dd489e6cd8db3f0f2bbc
Rodrigo Marques <rodrigo.marques@uniriotec.br>
2017-10-17 03:25:09 -0200

    Created main that generates random bus movement

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,9 +1,33 @@
 package code;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Olá Mundo");
+		Random randomNumber = new Random();
+		Route route = new Route(new Point(10, 10));
+		Line line = new Line(107, route);
+		Bus busOne = new Bus("bus1", line);
+		Bus busTwo = new Bus("bus2", line);
+		Bus busThree = new Bus("bus3", line);
+		Bus busFour = new Bus("bus4", line);
+		
+		busOne.move(randomNumber.nextInt(10), 0);
+		System.out.print("Start: ");
+		System.out.println("(" + busOne.getPosition().getX() + ", " + busOne.getPosition().getY() + ")");
+		for (int i = 0; i < 60; i++){
+			boolean moved = false;
+			do {
+				float x = randomNumber.nextInt(10);
+				float y = randomNumber.nextInt(10);
+				moved = busOne.move(x, y);
+				if (moved)
+					System.out.println(x + ", " + y);
+			} while (!moved);
+			System.out.print(i + ": ");
+			System.out.println("(" + busOne.getPosition().getX() + ", " + busOne.getPosition().getY() + ") - " + busOne.getLocation());
+		}
 	}
 
 }
