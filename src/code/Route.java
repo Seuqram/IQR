0d962f759e989b582acc23aeb264fa98ff3f74d7
Rodrigo Marques <rodrigo.marques@uniriotec.br>
2017-10-20 01:39:21 -0200

    Added javadoc and comments to Route

--- src/code/Route.java
+++ src/code/Route.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Represents a square route. The bus must pass through 4 limit points: (0,0)
- * (0,xMax) (xMax, yMax) (0, yMax)
- * 
+ * Represents the route that the bus travels.
+ * It has a set of points and the bus must pass through this set of points
  * @author Rodrigo
  * 
  */
@@ -14,23 +13,43 @@ public class Route
 {
 	private List<Point> points;
 
+	/**
+	 * Constructor with no arguments that instantiate a new ArrayList of points
+	 */
 	public Route()
 	{
-		this.points = new ArrayList<Point>();
+		this.points = new ArrayList<>();
 	}
 
+	/**
+	 * Method that creates a new point based on doubles latitute and longitude passed
+	 * and add this new point to the list of points of the route
+	 * @param double latitude
+	 * @param double longitude
+	 */
 	public void addPoint(float latitude, float longitude)
 	{
 		this.points.add(new Point(latitude, longitude));
 	}
 	
-	public int size()
+	/**
+	 * 
+	 * @return the quantity of points present in the route
+	 */
+	public int getPointsQuantity()
 	{
 		return points.size();
 	}
 
-	public Point getPoint(int index)
+	/**
+	 * 
+	 * @param index
+	 * @return the point, of the route's list of points, at the given index 
+	 */
+	public Point getPointAtIndex(int index)
 	{
-		return this.points.get(index);
+		//return a copy of the point not the reference to the point
+		Point point = this.points.get(index); 
+		return point;
 	}
 }
