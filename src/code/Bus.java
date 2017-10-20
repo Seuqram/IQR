ce4c9e2cb2a36cddbdf5465fe96f2c70e435288b
Rodrigo Marques <rodrigo.marques@uniriotec.br>
2017-10-20 01:48:31 -0200

    Added @Getter, changed constructor and move method
    Constructor changed to fit modifications in classes: point, line and route. Now the constructor uses double instead of float.
    Move method not ready, erased old method and it only returns false.

--- src/code/Bus.java
+++ src/code/Bus.java
@@ -1,11 +1,14 @@
 package code;
 
+import lombok.Getter;
+
 /**
  * Represents a bus
  * 
- * @author rodri
+ * @author rodrigo
  *
  */
+@Getter
 public class Bus
 {
 	private String identifier;
@@ -13,144 +16,25 @@ public class Bus
 	private Point position;
 
 	/**
-	 * 
-	 * @param identifier
-	 *            - Unique string to identify bus
-	 * @param line
-	 *            - Object from Line class
+	 * Bus constructor, receives a identifier that should be unique
+	 * and a line that contains a route. The start position of the bus
+	 * is the first point of the route of the line
+	 * @param identifier unique string to identify bus
+	 * @param line object from Line class
 	 */
 	public Bus(String identifier, Line line)
 	{
 		this.identifier = identifier;
 		this.line = line;
 		
-		float latitude = line.getRoute().getPoint(0).getLatitude();
-		float longitude = line.getRoute().getPoint(0).getLongitude();
+		//gets route first point and set this point as the start position of the bus
+		double latitude = this.line.getRoute().getPointAtIndex(0).getLatitude();
+		double longitude = this.line.getRoute().getPointAtIndex(0).getLongitude();
 		this.position = new Point(latitude, longitude);
-	}
-
-	public String getIdentifier()
-	{
-		return identifier;
-	}
-
-	public Line getLine()
-	{
-		return line;
-	}
-
-	/**
-	 * 
-	 * @return the point that represents the bus position
-	 */
-	public Point getPosition()
-	{
-		return position;
 	}
 
 	public boolean move(float distance)
 	{
-//		float busX = this.position.getLatitude();
-//		float busY = this.position.getLongitude();
-//		float maxX = this.getLine().getRoute().getMax().getLatitude();
-//		float maxY = this.getLine().getRoute().getMax().getLongitude();
-//		if (isValidMove(x, y))
-//		{
-//			if (isOnBottonOrLeftLocation())
-//			{
-//				this.position.setLatitude(busX + x);
-//				this.position.setLongitude(busY + y);
-//				this.location = getBusLocationBasedOnPosition();
-//				/*
-//				 * IF THE MOVE CHANGES THE POSITION AND KEEP MOVING THE NEW
-//				 * POSITION IS EQUAL: ACTUAL_POSITION + MOVEMENT - LIMIT
-//				 */
-//				if (busX + x > maxX)
-//				{
-//					this.position.setLatitude((busX + x)
-//							- (this.getLine().getRoute().getMax().getLatitude()));
-//				}
-//
-//				if (this.location == BUSLOCATION.TOP)
-//				{
-//					if (busY + y >= maxY)
-//					{
-//						if (busX + x > maxX)
-//						{
-//							this.position.setLatitude(maxX - x);
-//						}
-//					}
-//				}
-//				return true;
-//			} else
-//			{
-//				this.position.setLatitude(busX - x);
-//				this.position.setLongitude(busY - y);
-//				this.location = getBusLocationBasedOnPosition();
-//				return true;
-//			}
-//		}
 		return false;
 	}
-
-//	private boolean isValidMove(float x, float y)
-//	{
-//		float busX = this.position.getLatitude();
-//		float busY = this.position.getLongitude();
-//		float maxX = this.getLine().getRoute().getMax().getLatitude();
-//		float maxY = this.getLine().getRoute().getMax().getLongitude();
-//
-//		if (this.location == BUSLOCATION.BOTTON)
-//		{
-//			if (y > 0)
-//			{
-//				if (busX + x < maxX)
-//					return false;
-//			}
-//		}
-//		if (isOnBottonOrLeftLocation())
-//		{
-//			if ((x == 0) || (y == 0))
-//			{
-//				if (busX + x <= this.getLine().getRoute().getMax().getLatitude())
-//					if (busY + y <= this.getLine().getRoute().getMax().getLongitude())
-//						return true;
-//			} else if ((busX + x == this.getLine().getRoute().getMax().getLatitude())
-//					|| (busY + y == this.getLine().getRoute().getMax().getLongitude()))
-//				return true;
-//		} else
-//		{
-//			if ((x == 0) || (y == 0))
-//			{
-//				if (busX - x >= 0)
-//					if (busY - y >= 0)
-//						return true;
-//			} else if ((busX - x == 0) || (busY - y == 0))
-//				return true;
-//		}
-//		return false;
-//	}
-//
-//	private BUSLOCATION getBusLocationBasedOnPosition()
-//	{
-//		float BusX = this.position.getLatitude();
-//		float BusY = this.position.getLongitude();
-//		float MaxX = this.getLine().getRoute().getMax().getLatitude();
-//		float MaxY = this.getLine().getRoute().getMax().getLongitude();
-//
-//		if ((BusY == MaxY) && (BusX != 0))
-//			return BUSLOCATION.TOP;
-//		else if ((BusY != 0) && (BusX == 0))
-//			return BUSLOCATION.RIGHT;
-//		else if ((BusY == 0) && (BusX != MaxX))
-//			return BUSLOCATION.BOTTON;
-//		else
-//			return BUSLOCATION.LEFT;
-//	}
-//
-//	private boolean isOnBottonOrLeftLocation()
-//	{
-//		return ((this.location == BUSLOCATION.BOTTON)
-//				|| (this.location == BUSLOCATION.LEFT));
-//	}
 }
