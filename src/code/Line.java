25cd37c9bc46fe00307ec061687bab3725f4b0e0
Rodrigo Marques <rodrigo.marques@uniriotec.br>
2017-10-20 01:41:17 -0200

    Added @Getter and javadoc to Line

--- src/code/Line.java
+++ src/code/Line.java
@@ -1,23 +1,20 @@
 package code;
 
+import lombok.Getter;
+
+@Getter
 public class Line
 {
 	private int number;
 	private Route route;
 
+	/**
+	 * Constructor that receives a number and instantiate a new Route
+	 * @param number can not be changed after being set
+	 */
 	public Line(int number)
 	{
 		this.number = number;
 		this.route = new Route();
-	}
-
-	public int getNumber()
-	{
-		return number;
-	}
-
-	public Route getRoute()
-	{
-		return route;
 	}
 }
