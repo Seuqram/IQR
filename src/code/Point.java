09e6939a7749b069af42cc9948a18eb141b5f6c2
Rodrigo Marques <rodrigo.marques@visagio.com>
2017-10-22 22:00:48 -0200

    Added move function to Point

--- src/code/Point.java
+++ src/code/Point.java
@@ -27,26 +27,47 @@ public class Point
 	}
 	
 	public static Point calculatePoint(int distance, Point startPoint, Point endPoint){
-		double hypotenuse = startPoint.getDistanceToPoint(endPoint.getLatitude(), endPoint.getLongitude());
-		if (hypotenuse < distance)
-			return startPoint;
-		double oppositeLeg = endPoint.getLatitude();
-		double adjacentLeg = endPoint.getLongitude();		
-		double newHypotenuse = distance;
-		
-		if (startPoint.getLatitude() > endPoint.getLatitude()){
-			if (startPoint.getLongitude() > endPoint.getLongitude()){
-				newHypotenuse = hypotenuse - distance;
-				oppositeLeg = startPoint.getLatitude();
-				adjacentLeg = startPoint.getLongitude();
+		Point resultPoint = new Point(startPoint.getLatitude(), startPoint.getLongitude());
+		double distanceBetweenPoints = startPoint.getDistanceToPoint(endPoint.getLatitude(), endPoint.getLongitude());
+		if (distance <= distanceBetweenPoints){
+			if (startPoint.getLatitude() == endPoint.getLatitude()){
+				resultPoint.setLongitude(
+						startPoint.getLongitude() < endPoint.getLongitude() ? 
+								distance + startPoint.getLongitude() : 
+									startPoint.getLongitude() - distance);
+			}
+			else if (startPoint.getLongitude() == endPoint.getLongitude()){
+				resultPoint.setLatitude(
+						startPoint.getLatitude() < endPoint.getLatitude() ?
+								distance + startPoint.getLatitude() :
+									startPoint.getLatitude() - distance);
+			}else{
+				double newOppositeLeg = 0;
+				double newAdjacentLeg = 0;
+				if (startPoint.getLatitude() < endPoint.getLatitude()){
+					double proportionalLatitude = endPoint.getLatitude() - startPoint.getLatitude();
+					double patata = (proportionalLatitude * distance) / distanceBetweenPoints;
+					newOppositeLeg = patata + startPoint.getLatitude();
+				}
+				if (startPoint.getLatitude() > endPoint.getLatitude()){
+					double proportionalLatitude = startPoint.getLatitude() - endPoint.getLatitude();
+					double patata = (proportionalLatitude * distance) / distanceBetweenPoints;
+					newOppositeLeg = startPoint.getLatitude() - patata;
+				}
+				if (startPoint.getLongitude() < endPoint.getLongitude()){
+					double proportionalLongitude = endPoint.getLongitude() - startPoint.getLongitude();
+					double patati = (proportionalLongitude* distance) / distanceBetweenPoints;
+					newAdjacentLeg = startPoint.getLongitude() + patati;
+				}
+				if (startPoint.getLongitude() > endPoint.getLongitude()){
+					double proportionalLongitude = startPoint.getLongitude() - endPoint.getLongitude();
+					double patati = (proportionalLongitude* distance) / distanceBetweenPoints;
+					newAdjacentLeg = startPoint.getLongitude() - patati;
+				}
+				resultPoint = new Point(newOppositeLeg, newAdjacentLeg);
 			}
 		}
-		double sin = oppositeLeg / hypotenuse;
-		double cosin = adjacentLeg / hypotenuse;
-		double newOppositeLeg = sin * newHypotenuse;
-		double newAdjacentLeg = cosin * newHypotenuse;
-		Point point = new Point(newOppositeLeg, newAdjacentLeg);
-		return point;
+		return resultPoint;
 	}
 	
 }
--- src/test/TestPoint.java
+++ src/test/TestPoint.java
@@ -23,8 +23,6 @@ public class TestPoint {
 	
 	@Test
 	public void testGetDistanceToDifferentPoint() {
-		assertEquals(0, testPoint.getLatitude(), 0);
-		assertEquals(0, testPoint.getLongitude(), 0);
 		assertEquals(5, testPoint.getDistanceToPoint(5, 0));
 		assertEquals(9, testPoint.getDistanceToPoint(0, 9));
 		assertEquals(10, testPoint.getDistanceToPoint(5, 9));
@@ -32,43 +30,122 @@ public class TestPoint {
 	
 	@Test
 	public void testGetDistanceToSamePoint() {
-		assertEquals(0, testPoint.getLatitude(), 0);
-		assertEquals(0, testPoint.getLongitude(), 0);
 		assertEquals(0, testPoint.getDistanceToPoint(testPoint.getLatitude(), testPoint.getLongitude()));
 	}
 	
 	@Test
+	public void testGetPointUp(){
+		Point nextPoint = new Point(0, 10);
+		
+		//GOING UP FROM ZERO
+		Point resultPoint = Point.calculatePoint(5, testPoint, nextPoint);
+		assertEquals(0, resultPoint.getLatitude(), 0);
+		assertEquals(5, resultPoint.getLongitude(), 0);
+		
+		//GOIN UP FROM VALUE DIFFERENT FROM ZERO (5 IN THIS CASE)
+		testPoint.setLongitude(5);
+		resultPoint = Point.calculatePoint(5, testPoint, nextPoint);
+		assertEquals(0, resultPoint.getLatitude(), 0);
+		assertEquals(10, resultPoint.getLongitude(), 0);
+	}
+	
+	@Test
+	public void testGetPointForward(){
+		Point nextPoint = new Point(10,0);
+		
+		//GOING FORWARD FROM ZERO
+		Point resultPoint = Point.calculatePoint(5, testPoint, nextPoint);
+		assertEquals(5, resultPoint.getLatitude(), 0);
+		assertEquals(0, resultPoint.getLongitude(), 0);
+		
+		//GOIN UP FROM VALUE DIFFERENT FROM ZERO (5 IN THIS CASE)
+		testPoint.setLatitude(5);
+		resultPoint = Point.calculatePoint(5, testPoint, nextPoint);
+		assertEquals(10, resultPoint.getLatitude(), 0);
+		assertEquals(0, resultPoint.getLongitude(), 0);
+	}
+	
+	@Test
+	public void testGetPointDown(){
+		Point nextPoint = new Point(0,10);
+		
+		//GOING DOWN
+		Point resultPoint = Point.calculatePoint(5, nextPoint, testPoint);
+		assertEquals(0, resultPoint.getLatitude(), 0);
+		assertEquals(5, resultPoint.getLongitude(), 0);
+	}
+	
+	@Test
+	public void testGetPointBackward(){
+		Point nextPoint = new Point(10, 0);
+		
+		//GOIN BACK
+		Point resultPoint = Point.calculatePoint(5, nextPoint, testPoint);
+		assertEquals(5, resultPoint.getLatitude(), 0);
+		assertEquals(0, resultPoint.getLongitude(), 0);
+	}
+	
+	@Test
+	public void testGetPointUpForward(){
+		// POINT 9, 12 WILL FORM THE TRIANGLE WITH SIDES 9 12 15
+		Point nextPoint = new Point(9, 12);
+		
+		// GOING UP FORWARD FROM ZERO
+		Point resultPoint = Point.calculatePoint(5, testPoint, nextPoint);
+		assertEquals(3, resultPoint.getLatitude(), 0);
+		
+		// GOING UP FORWARD FROM POINT DIFFERENT FROM ZERO
+		testPoint = new Point(3, 4);
+		resultPoint = Point.calculatePoint(5, testPoint, nextPoint);
+		assertEquals(6, resultPoint.getLatitude(), 0);
+		assertEquals(8, resultPoint.getLongitude(), 0);
+		
+	}
+	
+	@Test
+	public void testGetPointDownForward(){
+		Point startPoint = new Point(5, 14);
+		Point endPoint = new Point(14, 2);
+		Point resultPoint = (Point.calculatePoint(5, startPoint, endPoint));
+		assertEquals(8, resultPoint.getLatitude(), 0);
+		assertEquals(10, resultPoint.getLongitude(), 0);
+		
+	}
+	
+	@Test
+	public void testGetPointDownBackward(){
+		Point startPoint = new Point(12, 16);
+		Point endPoint = new Point(3, 4);
+		Point resultPoint = Point.calculatePoint(5, startPoint, endPoint);
+		assertEquals(9, resultPoint.getLatitude(), 0);
+		assertEquals(12, resultPoint.getLongitude(), 0);
+	}
+	
+	@Test
+	public void testGetPoindUpBackward(){
+		Point startPoint = new Point(13, 7);
+		Point endPoint = new Point(4, 19);
+		Point resultPoint = Point.calculatePoint(5, startPoint, endPoint);
+		assertEquals(10, resultPoint.getLatitude(), 0);
+		assertEquals(11, resultPoint.getLongitude(), 0);
+	}
+	
+//	@Test
+//	public void testGetPointWithInvalidDistance(){
+//		Point startPoint = new Point(0, 0);
+//		Point endPoint = new Point(3, 4);
+//		Point resultPoint = Point.calculatePoint(6, startPoint, endPoint);
+//		assertEquals(startPoint.getLatitude(), resultPoint.getLatitude(), 0);
+//		assertEquals(startPoint.getLongitude(), resultPoint.getLongitude(), 0);
+//	}
+	
+	@Test
 	public void testGetPointWithInvalidDistance(){
-		assertEquals(0, testPoint.getLatitude(), 0);
-		assertEquals(0, testPoint.getLongitude(), 0);
-		Point endPoint = new Point(9, 12);
-		assertEquals(15, testPoint.getDistanceToPoint(endPoint.getLatitude(), endPoint.getLongitude()));
-		Point point = Point.calculatePoint(16, testPoint, endPoint);
-		assertEquals(testPoint.getLatitude(), point.getLatitude(), 0);
-		assertEquals(testPoint.getLongitude(), point.getLongitude(), 0);
+		Point startPoint = new Point (3,4);
+		Point endPoint = new Point (6,8);
+		Point resultPoint = Point.calculatePoint(12, startPoint, endPoint);
+		assertEquals(startPoint.getLatitude(), resultPoint.getLatitude(), 0);
+		assertEquals(startPoint.getLongitude(), resultPoint.getLongitude(), 0);
 	}
 	
-	@Test
-	public void testGetPointWithValidDistance(){
-		assertEquals(0, testPoint.getLatitude(), 0);
-		assertEquals(0, testPoint.getLongitude(), 0);
-		Point endPoint = new Point(9, 12);
-		assertEquals(15, testPoint.getDistanceToPoint(endPoint.getLatitude(), endPoint.getLongitude()));
-		Point point = Point.calculatePoint(5, testPoint, endPoint);
-		assertEquals(3, point.getLatitude(), 0);
-		assertEquals(4, point.getLongitude(), 0);
-	}
-	
-	
-	@Test
-	public void testGetPointWhilePointsReturn(){
-		assertEquals(0, testPoint.getLatitude(), 0);
-		assertEquals(0, testPoint.getLongitude(), 0);
-		Point endPoint = new Point(9, 12);
-		assertEquals(15, endPoint.getDistanceToPoint(testPoint.getLatitude(), testPoint.getLongitude()));
-		System.out.println("patati");
-		Point point = Point.calculatePoint(10, endPoint, testPoint);
-		assertEquals(3, point.getLatitude(), 0);
-		assertEquals(4, point.getLongitude(), 0);
-	}
 }
