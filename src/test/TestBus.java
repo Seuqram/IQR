fc9e7903a640a0ad9545526439cfa1933860cdfd
Rodrigo Marques <rodrigo.marques@uniriotec.br>
2017-10-26 01:27:16 -0200

    Modified TestBus to match new Bus' atributtes

--- src/test/TestBus.java
+++ src/test/TestBus.java
@@ -33,8 +33,8 @@ public class TestBus {
 		Point expectedPoint = new Point(
 				testLine.getRoute().getPointAtIndex(0).getLatitude(), 
 				testLine.getRoute().getPointAtIndex(0).getLongitude());
-		assertEquals(expectedPoint.getLatitude(), testBus.getPosition().getLatitude(), 0);
-		assertEquals(expectedPoint.getLongitude(), testBus.getPosition().getLongitude(), 0);
+		assertEquals(expectedPoint.getLatitude(), testBus.getCurrentPosition().getLatitude(), 0);
+		assertEquals(expectedPoint.getLongitude(), testBus.getCurrentPosition().getLongitude(), 0);
 	}
 	
 }
